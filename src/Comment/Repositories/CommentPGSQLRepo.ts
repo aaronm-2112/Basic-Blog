5487da3f704b1167ae20df28adb31c2eb1a4d0fa
aaronm-2112 <aaron.marroquin96@gmail.com>
2020-08-24 13:41:15 -0700

    Create code comment repo WIP

--- src/Comment/Repositories/CommentPGSQLRepo.ts
+++ src/Comment/Repositories/CommentPGSQLRepo.ts
@@ -29,12 +29,12 @@ export default class CommentPGSQLRepo implements ICommentRepository {
       if (reply) {
         //construct query that returns replies using the date as the primary means of ordering
         if (orderBy === 'date') {
-          query = `SELECT FROM comments * WHERE replyto = ${replyTo} AND cid > ${cid} ORDER BY date ASC, cid ASC LIMIT 10`;
+          query = `SELECT FROM comments * WHERE replyto = $1 AND cid > $2 ORDER BY date ASC, cid ASC LIMIT 10`;
           //add the query values to the query values collection
           queryValues.push(replyTo);
           queryValues.push(cid);
         } else {//return replies ordered by likes
-          query = `SELECT FROM comments * WHERE replyto = ${replyTo} AND ( likes, cid) < (${likes}, ${cid}) ORDER BY likes DESC, cid DESC LIMIT 10`;
+          query = `SELECT FROM comments * WHERE replyto = $1 AND ( likes, cid) < ($2, $3) ORDER BY likes DESC, cid DESC LIMIT 10`;
           //add the query values to the query values collection
           queryValues.push(replyTo);
           queryValues.push(likes);
@@ -46,8 +46,6 @@ export default class CommentPGSQLRepo implements ICommentRepository {
 
       }
 
-
-
       //execute the query
       let res = await this.pool.query(query, queryValues);
 
@@ -56,6 +54,39 @@ export default class CommentPGSQLRepo implements ICommentRepository {
 
       //return the results
       return comments;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  //TODO: Make values array more typescript. 
+  async create(comment: IComment): Promise<number> {
+    try {
+      //create the query -- created and cid should be auto-created columns
+      let query: string = `INSERT INTO comments username, blogid, content, reply, replyto, likes, deleted created VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+
+      //add the comment values
+      let values: any[] = [];
+      values.push(comment.username);
+      values.push(comment.blogid);
+      values.push(comment.content);
+      values.push(comment.reply);
+      values.push(comment.replyto);
+      values.push(comment.likes);
+      values.push(comment.deleted);
+
+      //execute the insertion
+      let result = await this.pool.query(query, values);
+
+      console.log(result);
+
+      //retrieve the last rowID from the result object -- lastID is only populated when we use an insert
+      let cid: number = result.rows[0]["cid"];
+
+      console.log(cid);
+
+      //returnt the comment id
+      return cid;
     } catch (e) {
       throw new Error(e);
     }
--- src/Comment/Repositories/ICommentRepository.ts
+++ src/Comment/Repositories/ICommentRepository.ts
@@ -5,4 +5,5 @@ import IComment from '../IComment';
 export default interface ICommentRepository {
   findAll(reply: boolean, replyTo: number, orderBy: string, likes: number, cid: number): Promise<Array<IComment>>; //returns top level comments ordered by likes, or date
   //findAllByDate(): Array<IComment>; //returns comments(top level or replies) ordered by date
+  create(comment: IComment): Promise<number>; //create comment return the cid
 }
